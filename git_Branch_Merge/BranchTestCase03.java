99bab175b94f773d0cefd54f37d3f155b2090d72
kms <godloveskidist@gmail.com>
2022-01-24 12:47:57 -0500

    changes from local intellij

--- git_Branch_Merge/BranchTestCase03.java
+++ git_Branch_Merge/BranchTestCase03.java
@@ -1,6 +1,7 @@
 package git_Branch_Merge;
 
 public class BranchTestCase03 {
-    //learning about conflict
+    //learning about conflict.
     //conflict while branching and merging
 }
+//this is the change from local intellij
