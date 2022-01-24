95d02da49507d04fe26769f00ca9a05a7c2cac98
kms <godloveskidist@gmail.com>
2022-01-24 10:45:45 -0500

    Branch test case started

--- git_Branch_Merge/Hello.java
+++ git_Branch_Merge/Hello.java
@@ -3,5 +3,6 @@ package git_Branch_Merge;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        //local master is the head
     }
 }
